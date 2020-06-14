2f910da1becbf9d769c2e9caca2f8206ace93deb
c-austin <cory.austin@ryerson.ca>
2020-06-14 17:07:47 -0300

    more stuffs

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,5 @@ let facebookBrowser = window.navigator.appVersion.includes('FB');
 let ios = window.navigator.appVersion.includes('iPhone');
 
 if (ios && facebookBrowser) {
-    document.getElementById('test').innerText = 'this is a redirect page';
+    
 }
-
-document.getElementById('test2').innerText = window.navigator.appVersion;
