8a5b9fa9945cfaf38438c3061d97c074e84bc577
c-austin <cory.austin@ryerson.ca>
2020-06-14 17:12:04 -0300

    complete

--- docs/script.js
+++ docs/script.js
@@ -2,5 +2,6 @@ let facebookBrowser = window.navigator.appVersion.includes('FB');
 let ios = window.navigator.appVersion.includes('iPhone');
 
 if (ios && facebookBrowser) {
-    
+    document.getElementById('main').classList.add('hidden');
+    document.getElementById('redirect').classList.remove('hidden');
 }
