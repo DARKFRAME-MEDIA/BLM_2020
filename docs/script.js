faf33c70f45d21b1df594c7533eeaef9e72a7669
c-austin <cory.austin@ryerson.ca>
2020-06-14 21:29:57 -0300

    copy functionality

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,13 @@
 const ANDROID_LINK = 'intent://arvr.google.com/scene-viewer/1.0?file=https://github.com/DARKFRAME-MEDIA/BLM2020-Android/blob/master/BLM2020.glb?raw=true#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;';
 const IOS_LINK = 'https://github.com/DARKFRAME-MEDIA/BLM/blob/master/blm2020.usdz?raw=true';
 
-
 let facebookBrowser = window.navigator.appVersion.includes('FB');
 let ios = window.navigator.appVersion.includes('iPhone');
 let mobile = true;
 
 if (mobile) {
-    if(ios) {
-        if(facebookBrowser) {
+    if (ios) {
+        if (facebookBrowser) {
             document.getElementById('mobileRedirect').classList.remove('hidden');
         } else {
             document.getElementById('main').classList.remove('hidden');
@@ -22,5 +21,17 @@ if (mobile) {
     document.getElementById('desktopRedirect').classList.remove('hidden');
 }
 
-
 document.getElementById('browserinfo').innerText = window.navigator.appVersion;
+
+
+document.getElementById('copyUrlButton').addEventListener('click', function (event) {
+    var temp = document.createElement('textarea');
+    temp.value = window.location.href;
+    temp.setAttribute('readonly', '');
+    temp.style = { position: 'absolute', left: '-9999px' };
+    document.body.appendChild(temp);
+    temp.select();
+    document.execCommand('copy');
+    document.body.removeChild(temp);
+
+});
